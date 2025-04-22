4d85ecf1987af07e20b7f0ff2b66f52f5c3d9d8e
ChaeYubin <cyoobin@naver.com>
2025-04-22 14:39:15 +0900

    feat: 목표, 할 일 핸들러 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,11 @@
 import { AuthHandlers } from "./v1/auth";
 import { DashboardHandlers } from "./v1/dashboard/todos";
+import { GoalHandlers } from "./v1/goal";
+import { TodoHandlers } from "./v1/todos";
 
-export const handlers = [...AuthHandlers, ...DashboardHandlers];
+export const handlers = [
+  ...AuthHandlers,
+  ...DashboardHandlers,
+  ...GoalHandlers,
+  ...TodoHandlers,
+];
